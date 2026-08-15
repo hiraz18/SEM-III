74f197b01a249bc9910e3e13866ce7df9d559db2
hiraz18 <zaidi.hira20@gmail.com>
2026-08-15 21:23:01 +0530

    Add task2.java to find the greatest of three numbers

---
+++ week-4/task2.java
@@ -0,0 +1,21 @@
+public class w3t1 {
+    public static void main(String[] args){
+        int a=1;
+        int b=2;
+        int c=3;
+        
+        if(a>=b && a>=c)
+        {
+            System.out.println(a+" is da greatest");
+        }
+        else if (b>=a && b>=c)
+        {
+            System.out.println(b+" is da greatest");
+        }
+        else
+        {
+            System.out.println(c+" is da greatest");
+        }
+    }
+    
+}
