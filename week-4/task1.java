a52ffecd70be4ae05b61679fddeabb3b29159dee
hiraz18 <zaidi.hira20@gmail.com>
2026-08-15 21:17:21 +0530

    Add even/odd check in task1.java

---
+++ week-4/task1.java
@@ -0,0 +1,14 @@
+public class w3t1 {
+    public static void main(String[] args){
+        int i=2;
+        if (i%2==0)
+        {
+            System.out.println(i+" is even");
+        }
+        else
+        {
+            System.out.println(i+" is odd");
+        }
+    }
+    
+}
