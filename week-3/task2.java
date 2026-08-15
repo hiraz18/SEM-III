fedf0fd0acb9f045245dfb08d614bec5fbc94a2a
hiraz18 <zaidi.hira20@gmail.com>
2026-08-15 19:13:23 +0530

    Add task2.java to calculate product of two floats

---
+++ week-3/task2.java
@@ -0,0 +1,9 @@
+public class w3t1 {
+    public static void main(String[] args){
+        float a=1.5f;
+        float b=2f;
+        float p=a*b;
+        System.out.println("the product is="+p);
+    }
+    
+}
