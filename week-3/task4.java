3df95792f7d73de37b965fc05deae07ac46ad171
hiraz18 <zaidi.hira20@gmail.com>
2026-08-15 19:19:36 +0530

    Add task4.java to calculate average of three numbers

---
+++ week-3/task4.java
@@ -0,0 +1,10 @@
+public class w3t1 {
+    public static void main(String[] args){
+        double a=2;
+        double b=3;
+        double c=1;
+        double average=(a+b+c)/3.0;
+        System.out.println(average);
+    }
+    
+}
