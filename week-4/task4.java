2165a9b28893c7fd7ef675988b6f9b78867874de
hiraz18 <zaidi.hira20@gmail.com>
2026-08-17 11:46:58 +0530

    Add NewClass1 to calculate sum of series

---
+++ week-4/task4.java
@@ -0,0 +1,9 @@
+public class NewClass1 {
+  public static void main(String[] args) {
+        int N = 10; 
+        long sum = (long) N * (N + 1) / 2;
+
+        System.out.println("The sum of the series 1 to " + N + " is: " + sum);
+    }
+}   
+ 
