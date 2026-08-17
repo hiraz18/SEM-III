e03de3857d07d2e902b68c9e2adb3e6d6c14ebea
hiraz18 <zaidi.hira20@gmail.com>
2026-08-17 11:42:32 +0530

    Add multiplication table program in task3.java

---
+++ week-4/task3.java
@@ -0,0 +1,9 @@
+public class NewClass1 {
+    public static void main(String[] args) {
+        int number = 8;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
