9df9d3b956878139c988733f14eaa61ddec5b23b
hiraz18 <zaidi.hira20@gmail.com>
2026-08-24 11:41:03 +0530

    Refactor class and update input handling

--- week-4/task5.java
+++ week-4/task5.java
@@ -1,14 +1,23 @@
-public class NewClass2 {
-    public static void main(String[] args) {
-        int number = 100; 
+import java.util.Scanner; 
 
-        System.out.println("Starting number: " + number);
-
-        while (number >= 10) {
-            number = number / 2; 
-            System.out.println("Current value: " + number);
-        }
+public class NewClass4 { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
         
-        System.out.println("Final value " + number + " is less than 10.");
-    }
-}   
+        System.out.print("Enter a starting number: "); 
+        int number = scanner.nextInt(); 
+        
+        System.out.println("Starting number: " + number); 
+        
+        while (number >= 10) { 
+            number = number / 2; 
+
+            System.out.println("Current value: " + number); 
+        } 
+        
+
+        System.out.println("Final value " + number + " is less than 10."); 
+        
+        scanner.close(); 
+    } 
+}
