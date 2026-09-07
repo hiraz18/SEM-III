71f75a33830f6493a7d7c23d4ce645e21841a911
hiraz18 <zaidi.hira20@gmail.com>
2026-09-07 11:52:10 +0530

    Create task1.java

---
+++ week-6/task1.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject2;
+public class Mavenproject2 {
+    public static void main(String[]args){
+        int n=99;
+        for (int i=1;i<=n;i+=2){
+            System.out.println(i);
+        }
+    } 
+}
